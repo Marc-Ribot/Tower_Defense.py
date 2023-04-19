9c5439c1b4df6611bd4ef688000769d0008a5029
Marc Ribot <marc.ribot.forns@estudiantat.upc.edu>
2023-04-19 16:12:16 +0200

    primdr commit1

---
+++ Classes/Segment.py
@@ -0,0 +1,10 @@
+from Recta import*
+
+
+class Segmento(Recta):
+    def longitud(self):
+      return self.P.distancia(self.Q)
+
+
+if __name__ == "__main__":
+    s = Segmento(Punto([0, 0]), Punto([]))
